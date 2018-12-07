63ec451a975862fdcea2a09111e9923411fc0d38
gfranco42 <franco.guillaume83@gmail.com>
2018-12-07 17:06:02 +0100

    update

--- manipulate.c
+++ manipulate.c
@@ -6,7 +6,7 @@
 /*   By: gfranco <gfranco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 11:23:34 by gfranco           #+#    #+#             */
-/*   Updated: 2018/12/07 16:38:28 by gfranco          ###   ########.fr       */
+/*   Updated: 2018/12/07 17:05:59 by gfranco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ void	redraw_move(t_m *m, float a, float b)
 	mlx_put_image_to_window(m->ptr, m->win, m->img, 0, 0);
 	m->xinit += a;
 	m->yinit += b;
-	m->x1 = m->xinit;
-	m->y1 = m->yinit;
 	draw_rot(*m);
 	mlx_put_image_to_window(m->ptr, m->win, m->img, 0, 0);
 }
@@ -54,8 +52,6 @@ void	iso(t_m *m)
 	m->savegap = m->initgap;
 	m->xinit = WIDTH / 2;
 	m->yinit = HEIGHT / 4;
-	m->x1 = m->xinit;
-	m->y1 = m->yinit;
 	draw_rot(*m);
 	mlx_put_image_to_window(m->ptr, m->win, m->img, 0, 0);
 }
@@ -71,8 +67,6 @@ void	paralelle(t_m *m)
 	m->xtheta = 0;
 	m->ytheta = 0;
 	m->ztheta = 0;
-	m->x1 = m->xinit;
-	m->y1 = m->yinit;
 	draw_rot(*m);
 	mlx_put_image_to_window(m->ptr, m->win, m->img, 0, 0);
 }
@@ -85,8 +79,6 @@ void	conique(t_m *m)
 	m->savegap = m->initgap;
 	m->xinit = WIDTH / 2 - ((m->column - 1) * m->gap) / 2;
 	m->yinit = HEIGHT / 2 - ((m->line - 1) * m->gap) / 2;
-	m->x1 = m->xinit;
-	m->y1 = m->yinit;
 	draw_rot(*m);
 	mlx_put_image_to_window(m->ptr, m->win, m->img, 0, 0);
 }
